5971e7d80cc16a412b3acfe5b87cdc6b7a79efe4
ImamSyabana <imambritish@gmail.com>
2025-03-05 11:33:56 +0700

    debugger

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,18 @@
-age = int(input("How old are you?"))
+# try:
+#     age = int(input("How old are you?"))
+#
+# except ValueError:
+#     while age.isnumeric() != True:
+#
+#         print("Please enter a valid number: ")
+#         age = int(input("How old are you?"))
+
+while True:
+    try:
+        age = int(input("How old are you? "))  # Try to get a valid integer
+        break  # If successful, exit loop
+    except ValueError:
+        print("Please enter a valid number.")  # Prompt again if input is invalid
+
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
